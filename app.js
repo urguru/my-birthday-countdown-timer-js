045a2c259c1ff834f6d67bc6d15c6ca1f2649658
urguru <39453452+urguru@users.noreply.github.com>
2019-07-13 08:44:37 +0530

    Birthday Countdown timer

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 function countDown() {
   // The variable now holds the current date
-  var now = new Date();
-  //.getDay() gives which day it is ie 0-Sunday 1-Monday 2-Tuesday...6-Saturday
-  //So if its saturday today then we will have 6 days and some hours remaining
-  //If its friday then we will have 0 days and some hours remaining
-  //If its thursday then we will have 1 days and some hours remaining
-  //If its sunday then we will have 5 days and some hours remaining for nxt saturday
-  //The variable d holds that value
-  d = now.getDay() % 7;
-  //Remaining hours for next day will be the current time subtracted from 23 hours and not from 24 hours
-  //Because the remaining one hour is being divided into minutes and seconds
-  h = 23 - now.getHours();
-  //The same condition for the remaining minutes in an hour we subtract from 59 and not from 60
-  m = 59 - now.getMinutes();
-  s = 59 - now.getSeconds();
-  //This is for better styling
+  var now = new Date().getTime();
+  var eventDate = new Date("Sept 11, 2019 00:00:00").getTime();
+
+  var diffDate = eventDate - now;
+
+  s = Math.floor(diffDate / 1000) % 60;
+  m = Math.floor(diffDate / 60000) % 60;
+  h = Math.floor(diffDate / (60000 * 60)) % 24;
+  d = Math.floor(diffDate / (60000 * 60 * 24));
+
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
